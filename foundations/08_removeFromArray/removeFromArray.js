1b07030326caa17624a95bdd34b44f8db6746bc4
Bogdan V. Brinzey <bbrinzey@gmail.com>
2026-03-14 00:17:47 +0500

    Solve 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+  const itemsSet = new Set(items);
+  return array.filter(x => !itemsSet.has(x));
 };
 
 // Do not edit below this line
