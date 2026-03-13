18ac9d64e07c93a3e3566d9c40207b20c29e0f22
Bogdan V. Brinzey <bbrinzey@gmail.com>
2026-03-14 00:36:50 +0500

    Solve 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
 
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+
+  if (a > b) {
+    [a, b] = [b, a];
+  }
+
+  return ((b + a)*(b - a + 1)) / 2;
 };
 
 // Do not edit below this line
