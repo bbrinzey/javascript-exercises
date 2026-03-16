b0f46d29fafe8d76277cd708fa9431e6654c2c8a
Bogdan V. Brinzey <bbrinzey@gmail.com>
2026-03-16 21:56:51 +0500

    Solve 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (typeof n !== "number") {
+    n = parseInt(n);
+  }
 
+  if (n < 0) {
+    return "OOPS";
+  }
+
+  let [a, b] = [0, 1];
+  for (let i = 0; i < n; i++) {
+    [a, b] = [b, a + b];
+  }
+  return a;
 };
 
 // Do not edit below this line
