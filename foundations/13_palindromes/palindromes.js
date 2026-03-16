ac31e16f8ea8dc2bf8b1ac944eafe4a951d16f0b
Bogdan V. Brinzey <bbrinzey@gmail.com>
2026-03-16 21:50:39 +0500

    Solve 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleaned = str.match(/\w|\d/g).map((c) => c.toLowerCase());
+  return cleaned.join("") === cleaned.reverse().join("");
 };
 
 // Do not edit below this line
